fafa315240d717ad0010f435b975f8210c6132c2
Jerry <s1072750@gm.pu.edu.tw>
2021-03-12 15:10:11 +0800

    加入master分支程式碼

--- app/src/main/java/com/example/hello2021/MainActivity.kt
+++ app/src/main/java/com/example/hello2021/MainActivity.kt
@@ -8,9 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello tcyang"
-
-
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = “Hello tcyang”
+        TestMaster()
     }
+
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
+    }
+
+
+
+}
 }
